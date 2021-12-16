7e5eacc75b0452f38c47ad990af8f59c9f036f0a
Julian Brost <julian.brost@icinga.com>
2021-12-16 14:39:56 +0100

    InfluxdbCommonWriter: only flush from work queue
    There is no explicit synchronization of access to m_DataBuffer which is fine if
    it is only accessed from the single-threaded work queue. However, Stop() also
    called Flush() in another thread, leading to concurrent write access to
    m_DataBuffer which can result in a crash due to use after free/double free.
    
    Changes in this commit:
    * Flush() is renamed to FlushWQ() to show that it should only be called from
      the work queue. Additionally, it now asserts that it is running on the work
      queue.
    * Visibility of some data members is changed from protected to private. No
      other classes have to access these at the moment. By this change, accidental
      concurrent access from derived classes in the future is prevented.
    * Stop() now flushes by posting FlushWQ() to the work queue and joining it.

--- lib/perfdata/influxdbcommonwriter.cpp
+++ lib/perfdata/influxdbcommonwriter.cpp
@@ -109,19 +109,12 @@ void InfluxdbCommonWriter::Pause()
 	Log(LogDebug, GetReflectionType()->GetName())
 		<< "Flushing pending data buffers.";
 
-	Flush();
+	m_WorkQueue.Enqueue([this]() {
+		FlushWQ();
+	}, PriorityHigh);
 
-	/* Work on the missing tasks. TODO: Find a way to cache them on disk. */
-	Log(LogDebug, GetReflectionType()->GetName())
-		<< "Joining existing WQ tasks.";
-
+	/* Wait for the flush to complete, implicitly waits for all WQ tasks enqueued prior to pausing. */
 	m_WorkQueue.Join();
-
-	/* Flush again after the WQ tasks have filled the data buffer. */
-	Log(LogDebug, GetReflectionType()->GetName())
-		<< "Flushing data buffers from WQ tasks.";
-
-	Flush();
 
 	Log(LogInformation, GetReflectionType()->GetName())
 		<< "'" << GetName() << "' paused.";
@@ -418,7 +411,7 @@ void InfluxdbCommonWriter::SendMetric(const Checkable::Ptr& checkable, const Dic
 			<< "Data buffer overflow writing " << m_DataBuffer.size() << " data points";
 
 		try {
-			Flush();
+			FlushWQ();
 		} catch (...) {
 			/* Do nothing. */
 		}
@@ -437,11 +430,13 @@ void InfluxdbCommonWriter::FlushTimeoutWQ()
 	Log(LogDebug, GetReflectionType()->GetName())
 		<< "Timer expired writing " << m_DataBuffer.size() << " data points";
 
-	Flush();
+	FlushWQ();
 }
 
-void InfluxdbCommonWriter::Flush()
+void InfluxdbCommonWriter::FlushWQ()
 {
+	AssertOnWorkQueue();
+
 	namespace beast = boost::beast;
 	namespace http = beast::http;
 
